038b7748825dec6dc050cb2d8960d4fbb3f7bfa3
Hekk_Elek <horvath.kristof.sz@gmail.com>
2023-11-20 20:05:21 +0100

    Hópehely panelized.

--- hw_hopehely/create_panels.cmd
+++ hw_hopehely/create_panels.cmd
@@ -1,12 +1,11 @@
 @echo off
 
 kikit panelize ^
---layout "grid; rows:3; cols:7; vspace:5mm; hspace:-9mm; alternation:rowsCols; rotation:0deg; hbackbone:5mm;" ^
+--layout "grid; rows:3; cols:4; vspace:3mm; hspace:3mm; alternation:none; rotation:0deg; hbackbone:5mm;" ^
 --tabs "annotation" ^
 --framing "frame; width:5mm; space:3mm; cuts:h" ^
---cuts "mousebites; drill:0.5mm; spacing: 1mm; offset:-0.5mm; prolong:0.5mm" ^
+--cuts "mousebites; drill:0.5mm; spacing: 0.8mm; offset:-0.5mm; prolong:0.5mm" ^
 --source "tolerance:20mm" ^
 --post "millradius:0.5mm; copperfill: true" ^
-karifa.kicad_pcb panel.kicad_pcb
+hopehely.kicad_pcb panel.kicad_pcb
     
-REM --tabs 'fixed; width: 3mm; height: 3mm; vcount: 1; hcount: 1' \
